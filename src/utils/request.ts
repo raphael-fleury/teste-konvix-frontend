1d83f82f5c9869d534ddc53fa1f2e72eaf2957d1
Raphael Fleury <raphael.fleury9@gmail.com>
2024-02-15 01:54:14 -0300

    Axios with credentials

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import { logout } from "./logout";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000"
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000",
+    withCredentials: true
 })
 
 api.interceptors.response.use(
